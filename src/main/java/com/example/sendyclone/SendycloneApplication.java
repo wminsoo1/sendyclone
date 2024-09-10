1f716ed75713e0116ddede3b9599136dfcdece3a
wminsoo1 <wminsoo1@naver.com>
2024-09-10 13:27:45 +0900

    Initial commit

---
+++ src/main/java/com/example/sendyclone/SendycloneApplication.java
@@ -0,0 +1,13 @@
+package com.example.sendyclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendycloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SendycloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sendyclone/SendycloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sendyclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SendycloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
